16bba7a44d5909ce65ef943a1a73e797fef42cb2
Калин Марков <kalin_markoff@mail.bg>
2018-01-02 01:02:59 +0200

    Moved My Blog-Site in new repo

---
+++ AutoTrader/AutoTrader/Data/EngineType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoTrader.Data
+{
+    public enum EngineType
+    {
+        Petrol,
+        Diesel,
+        Electrical,
+        None
+    }
+}
